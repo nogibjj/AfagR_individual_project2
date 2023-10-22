6d7e9bd6d8964f93d07aad16024c1620b64a067b
johncoogan53 <141885470+johncoogan53@users.noreply.github.com>
2023-10-22 00:40:58 +0000

    updates, not going to pass

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,101 @@
-//create an add function that takes two numbers and returns the sum
+use csv::Reader;
+use rusqlite::{Connection, Result, MappedRows};
 
+
+pub fn create_table(conn: &Connection) -> Result<()> {
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS game_sales (
+             Rank INTEGER,
+             Name TEXT,
+             Platform TEXT,
+             Publisher TEXT,
+             Developer TEXT,
+             Critic_Score REAL,
+             User_Score REAL,
+             Total_Shipped REAL,
+             Year REAL
+        )",
+        [],
+    )?;
+    Ok(())
+}
+
+pub fn import_csv_to_sqlite(conn: &Connection) -> Result<()> {
+    let mut reader = match Reader::from_path("src/game_sales_data.csv") {
+        Ok(reader) => reader,
+        Err(err) => {
+            println!("Error reading file: {:?}", err);
+            return Ok(());
+        }
+    };
+
+    // Prepare an SQL statement outside the loop for better performance
+    //let _stmt = conn.prepare("INSERT INTO game_sales (Rank, Name, Platform, Publisher, Developer, Critic_Score, User_Score, Total_Shipped, Year) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9)")?;
+
+    let mut batch_data = String::new();
+
+    for result in reader.byte_records() {
+        let record = match result {
+            Ok(record) => record,
+            Err(err) => {
+                println!("Error reading record: {:?}", err);
+                continue;
+            }
+        };
+        
+        let rank = String::from_utf8_lossy(&record[0]);
+        let name = String::from_utf8_lossy(&record[1]);
+        let platform = String::from_utf8_lossy(&record[2]);
+        let publisher = String::from_utf8_lossy(&record[3]);
+        let developer = String::from_utf8_lossy(&record[4]);
+        let mut critic_score = String::from_utf8_lossy(&record[5]);
+        let mut user_score = String::from_utf8_lossy(&record[6]);
+        let total_shipped = String::from_utf8_lossy(&record[7]);
+        let year = String::from_utf8_lossy(&record[8]);
+
+        let name_escaped = name.replace('\'', "''");
+        let platform_escaped = platform.replace('\'', "''");
+        let publisher_escaped = publisher.replace('\'', "''");
+        let developer_escaped = developer.replace('\'', "''");
+        if user_score == ""{
+            user_score = std::borrow::Cow::Borrowed("NULL");
+        }
+        if critic_score == ""{
+            critic_score = std::borrow::Cow::Borrowed("NULL");
+        }
+
+        let curr_query = format!(
+            "INSERT INTO game_sales (Rank, Name, Platform, Publisher, Developer, Critic_Score, User_Score, Total_Shipped, Year) VALUES ({}, '{}', '{}', '{}', '{}', {}, {}, {}, {});",
+            rank, name_escaped, platform_escaped, publisher_escaped, developer_escaped, critic_score, user_score, total_shipped, year
+        );
+        batch_data.push_str(&curr_query);
+        log::debug!("Current Query: {curr_query}");
+        const BATCH_SIZE: usize = 9999999999999999999;
+        if batch_data.len() >= BATCH_SIZE {
+            log::debug!("Batch Data: {batch_data}");
+            conn.execute_batch(&batch_data)?;
+            log::debug!("Batch Data Executed");
+            batch_data.clear();
+        }
+    }
+    log::debug!("reached the end of the loop");
+    //if !batch_data.is_empty() {
+        //conn.execute_batch(&batch_data)?;}
+    log::debug!("reached the end of the load csv");
+
+    Ok(())
+}
+
+// make a function that queries the database and returns the result with a string as the argument
+
+fn query_database<'a>(conn: &'a Connection, query: &str) -> Result<Vec<rusqlite::Row<'a>>> {
+    let mut stmt = conn.prepare(query)?;
+    let rows = stmt.query_map([],|row| row)?;
+
+    let result:Result<Vec<rusqlite::Row>> = rows.collect();
+
+    Ok(result)
+}
 pub fn add(a: i32, b: i32) -> i32 {
     a + b
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,42 @@
 //make a main function that calls the add function in lib and prints the result
 //import add from lib
 
-use steam_cli::add;
+use std::time::Instant;
+use steam_cli::{create_table, import_csv_to_sqlite, query_db};
 
+struct Profiler {
+    start_time: Instant,
+}
+
+impl Profiler {
+    fn new() -> Profiler {
+        Profiler {
+            start_time: Instant::now(),
+        }
+    }
+
+    fn start(&mut self) {
+        self.start_time = Instant::now();
+    }
+
+    fn stop(&self, name: &str) {
+        let elapsed = self.start_time.elapsed();
+        println!("{} took {:?}", name, elapsed);
+    }
+}
 fn main() {
-    println!("Hello, world!");
-    println!("1 + 2 = {}", add(1, 2));
+    env_logger::init();
+    let mut profiler = Profiler::new();
+    profiler.start();
+    //run lib create table and import csv to sqlite
+    let conn = rusqlite::Connection::open("game_sales.db").unwrap();
+    create_table(&conn).unwrap();
+    profiler.stop("create table");
+    profiler.start();
+    import_csv_to_sqlite(&conn).unwrap();
+    profiler.stop("import csv to sqlite");
+    profiler.start();
+    let result = query_db(&conn, "SELECT * FROM game_sales LIMIT 5").unwrap();
+    profiler.stop("query db");
+    println!("{:?}", result);
 }
